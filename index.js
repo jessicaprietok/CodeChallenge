62c2424ec2daadbf1cd9b275f36dff7d6a71e245
Jessica Prieto <jessicapr1202@gmail.com>
2023-09-09 17:10:22 -0300

    Corrección error

--- index.js
+++ index.js
@@ -26,3 +26,46 @@ document.getElementById('datos').addEventListener('submit', function(event){
 
 
 });
+
+
+document.getElementById('datos').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const nombre = document.getElementById('nombre').value;
+    const apellido = document.getElementById('apellido').value;
+    const fecha = document.getElementById('fecha').value;
+    const datos = {
+        nombre: nombre,
+        apellido: apellido,
+        fecha_nacimiento: fecha
+    };
+
+
+    const URL = "https://jsonplaceholder.typicode.com/users";
+
+    const solicitudPost = {
+        method: "POST",
+        body: JSON.stringify(datos),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    fetch(URL, solicitudPost)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No se pudo enviar la data");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Nuevo usuario creado", data);
+            alert("¡La solicitud se realizó correctamente!");
+            document.getElementById('fname').value = '';
+            document.getElementById('lname').value = '';
+            document.getElementById('bday').value = '2011-11-22';
+        })
+        .catch(error => {
+            console.error("Error", error);
+        });
+});
